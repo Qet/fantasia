86b182cdb65fa3a446f400813a3bd0ceb622237a
Rhett Winston <rhettius@gmail.com>
2018-11-13 07:45:42 +1100

    fixed TCP stack, commands now being processed.

--- ports/socketmanager.cs
+++ ports/socketmanager.cs
@@ -59,7 +59,7 @@ namespace ports {
         }
 
         private void createNewStack(TcpClient client, int portID){
-            Packetiser newPacketiser = new Packetiser(codec, "/r");
+            Packetiser newPacketiser = new Packetiser(codec, "\r\n");
             TCPSocket newSocket = new TCPSocket(portID, client, newPacketiser);
 
             packetisers.Add(newPacketiser);
