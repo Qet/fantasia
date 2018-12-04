64d595de1489698bf6be90edfb2d900731280b62
Rhett Winston <rhettius@gmail.com>
2018-12-05 06:51:36 +1100

    finally fixed the test runner script

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
-#!/bin/sh +e
+#!/bin/bash +e
 
 # Run all the tests
 
-pushd `pwd`
+pushd $PWD
+START_DIR=$PWD
 for x in `ls -d *.tests`; do
     cd $x
     dotnet test
+    cd $START_DIR
 done;
+popd
 
